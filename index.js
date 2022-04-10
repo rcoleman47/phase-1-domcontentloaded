9e6102abfdfb085fa3c414c4e4e46838caa41b8e
ryanadams <rcoleman.adams@gmail.com>
2022-04-10 18:00:40 -0500

    commit tree ee5c149f61fd212a1a414504ac8aaad416b3dbd2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", changeID)
+
+function changeID (){
+    const text = document.getElementById('text');
+    console.log(text.textContent = "This is really cool!")
+}
+console.log('first')
